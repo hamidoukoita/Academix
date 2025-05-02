4384ed276e2040910ad287ac5d505b7d99698965
Midouzer <hamidoulamine10@gmail.com>
2025-05-02 11:01:18 +0000

    Ajout de nouvelles modification concernant notamment le middleware

--- routes/web.php
+++ routes/web.php
@@ -11,32 +11,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/admin', function () {
-        return 'Bienvenue sur le tableau de bord ADMIN';
-    });
-
-    Route::get('/enseignant', function () {
-        return 'Bienvenue sur le tableau de bord ENSEIGNANT';
-    });
-
-    Route::get('/etudiant', function () {
-        return 'Bienvenue sur le tableau de bord ÉTUDIANT';
-    });
+Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin', function () {
         return view('admin.dashboard');
     });
+});
 
+Route::middleware(['auth', 'role:enseignant'])->group(function () {
     Route::get('/enseignant', function () {
         return view('enseignant.dashboard');
     });
+});
 
+Route::middleware(['auth', 'role:etudiant'])->group(function () {
     Route::get('/etudiant', function () {
         return view('etudiant.dashboard');
     });
 });
+
 
 require __DIR__.'/auth.php';
